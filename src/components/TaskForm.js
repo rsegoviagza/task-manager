8f0b81db800dfa38384e40b872df7b0897a6ad0e
sega <rsegovia.gza@gmail.com>
2020-10-14 19:36:43 -0500

    Initial Version

---
+++ src/components/TaskForm.js
@@ -0,0 +1,57 @@
+import React, { useContext, useState, useEffect } from "react";
+import { TaskListContext } from "../context/TaskListContext";
+
+const TaskForm = () => {
+  const [title, setTitle] = useState("");
+  const { addTask, clearList, editItem, editTask } = useContext(
+    TaskListContext
+  );
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!editItem) {
+      addTask(title);
+      setTitle("");
+    } else {
+      editTask(title, editItem.id);
+    }
+  };
+
+  useEffect(() => {
+    if (editItem !== null) {
+      setTitle(editItem.title);
+      console.log(editItem);
+    } else {
+      setTitle("");
+    }
+  }, [editItem]); // Solo se ejecuta cuando editItem se actualiza
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        onChange={handleChange}
+        value={title}
+        type="text"
+        className="task-input"
+        placeholder="Add task..."
+        required
+      />
+      <div className="buttons">
+        <button type="submit" className="btn add-task-btn">
+          {editItem ? "Edit Task" : "Add Task"}
+        </button>
+        <button onClick={clearList} className="btn clear-btn">
+          Clear
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default TaskForm;
